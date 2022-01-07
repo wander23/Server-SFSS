f152a669fbeb1a5f6b43798ef46d3dfff38f47b0
wander23 <57674214+wander23@users.noreply.github.com>
2022-01-07 20:09:34 +0700

    Update PhotoController.java

--- mongodb/src/main/java/com/example/mongodb/photo/PhotoController.java
+++ mongodb/src/main/java/com/example/mongodb/photo/PhotoController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -36,8 +37,13 @@ public class PhotoController {
     ResponseEntity<ResponeObject> getImageOfUser(@PathVariable String id)
     {
         List<Photo> foundAll = photoResponsitory.findByIdUser(id);
+        List<String> urlImage = new ArrayList<>();
+        for (Photo i: foundAll)
+        {
+            urlImage.add(i.getUrlImage());
+        }
         return ResponseEntity.status(HttpStatus.OK).body(
-                new ResponeObject(foundAll)
+                new ResponeObject(urlImage)
         );
     }
 
